6a2678c82e27c7ea30b4bba91481c3295f713ced
Peter Tseng <pht24@cornell.edu>
2018-12-01 09:14:21 +0000

    md5: unroll loops
    day 04 1.5 -> 0.9 seconds

--- src/md5.rs
+++ src/md5.rs
@@ -1,35 +1,7 @@
+// Some ideas from https://openwall.info/wiki/people/solar/software/public-domain-source-code/md5
+// https://gist.github.com/lifthrasiir/7501753
+
 #![allow(clippy::unreadable_literal)]
-
-const K: [u32; 64] = [
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
-];
-
-#[rustfmt::skip]
-const S : [u8; 64] = [
-    7, 12, 17, 22,
-    7, 12, 17, 22,
-    7, 12, 17, 22,
-    7, 12, 17, 22,
-    5, 9, 14, 20,
-    5, 9, 14, 20,
-    5, 9, 14, 20,
-    5, 9, 14, 20,
-    4, 11, 16, 23,
-    4, 11, 16, 23,
-    4, 11, 16, 23,
-    4, 11, 16, 23,
-    6, 10, 15, 21,
-    6, 10, 15, 21,
-    6, 10, 15, 21,
-    6, 10, 15, 21,
-];
 
 const BLOCK_SIZE: usize = 512;
 const LENGTH_BITS: usize = 64;
@@ -44,6 +16,24 @@ impl Default for MD5 {
         Self::new()
     }
 }
+
+macro_rules! mix(
+    (F:  $x:expr, $y:expr, $z:expr) => ($z ^ ($x & ($y ^ $z)));
+    (G:  $x:expr, $y:expr, $z:expr) => ($y ^ ($z & ($x ^ $y)));
+    (H:  $x:expr, $y:expr, $z:expr) => (($x ^ $y) ^ $z);
+    (H2: $x:expr, $y:expr, $z:expr) => ($x ^ ($y ^ $z));
+    (I:  $x:expr, $y:expr, $z:expr) => ($y ^ ($x | !$z))
+);
+
+/*
+ * The MD5 transformation for all four rounds.
+ */
+macro_rules! step(($f:ident: $a:expr, $b:expr, $c:expr, $d:expr,
+                   $x:expr, $t:expr, $s:expr) => ({
+    $a = $a.wrapping_add(mix!($f: $b, $c, $d)).wrapping_add($x).wrapping_add($t);
+    $a = $a.rotate_left($s);
+    $a = $a.wrapping_add($b);
+}));
 
 impl MD5 {
     pub fn new() -> Self {
@@ -106,22 +96,75 @@ impl MD5 {
             let mut b = b0;
             let mut c = c0;
             let mut d = d0;
-            for i in 0..64 {
-                let (f, g) = if i <= 15 {
-                    ((b & c) | (!b & d), i)
-                } else if 16 <= i && i <= 31 {
-                    ((d & b) | (!d & c), (5 * i + 1) % 16)
-                } else if 32 <= i && i <= 47 {
-                    (b ^ c ^ d, (3 * i + 5) % 16)
-                } else {
-                    (c ^ (b | !d), (7 * i) % 16)
-                };
-                let f = f.wrapping_add(a).wrapping_add(K[i]).wrapping_add(m[g]);
-                a = d;
-                d = c;
-                c = b;
-                b = b.wrapping_add(f.rotate_left(u32::from(S[i])));
-            }
+
+            step!(F: a, b, c, d, m[0], 0xd76aa478, 7);
+            step!(F: d, a, b, c, m[1], 0xe8c7b756, 12);
+            step!(F: c, d, a, b, m[2], 0x242070db, 17);
+            step!(F: b, c, d, a, m[3], 0xc1bdceee, 22);
+            step!(F: a, b, c, d, m[4], 0xf57c0faf, 7);
+            step!(F: d, a, b, c, m[5], 0x4787c62a, 12);
+            step!(F: c, d, a, b, m[6], 0xa8304613, 17);
+            step!(F: b, c, d, a, m[7], 0xfd469501, 22);
+            step!(F: a, b, c, d, m[8], 0x698098d8, 7);
+            step!(F: d, a, b, c, m[9], 0x8b44f7af, 12);
+            step!(F: c, d, a, b, m[10], 0xffff5bb1, 17);
+            step!(F: b, c, d, a, m[11], 0x895cd7be, 22);
+            step!(F: a, b, c, d, m[12], 0x6b901122, 7);
+            step!(F: d, a, b, c, m[13], 0xfd987193, 12);
+            step!(F: c, d, a, b, m[14], 0xa679438e, 17);
+            step!(F: b, c, d, a, m[15], 0x49b40821, 22);
+
+            step!(G: a, b, c, d, m[1], 0xf61e2562, 5);
+            step!(G: d, a, b, c, m[6], 0xc040b340, 9);
+            step!(G: c, d, a, b, m[11], 0x265e5a51, 14);
+            step!(G: b, c, d, a, m[0], 0xe9b6c7aa, 20);
+            step!(G: a, b, c, d, m[5], 0xd62f105d, 5);
+            step!(G: d, a, b, c, m[10], 0x02441453, 9);
+            step!(G: c, d, a, b, m[15], 0xd8a1e681, 14);
+            step!(G: b, c, d, a, m[4], 0xe7d3fbc8, 20);
+            step!(G: a, b, c, d, m[9], 0x21e1cde6, 5);
+            step!(G: d, a, b, c, m[14], 0xc33707d6, 9);
+            step!(G: c, d, a, b, m[3], 0xf4d50d87, 14);
+            step!(G: b, c, d, a, m[8], 0x455a14ed, 20);
+            step!(G: a, b, c, d, m[13], 0xa9e3e905, 5);
+            step!(G: d, a, b, c, m[2], 0xfcefa3f8, 9);
+            step!(G: c, d, a, b, m[7], 0x676f02d9, 14);
+            step!(G: b, c, d, a, m[12], 0x8d2a4c8a, 20);
+
+            step!(H: a, b, c, d, m[5], 0xfffa3942, 4);
+            step!(H2: d, a, b, c, m[8], 0x8771f681, 11);
+            step!(H: c, d, a, b, m[11], 0x6d9d6122, 16);
+            step!(H2: b, c, d, a, m[14], 0xfde5380c, 23);
+            step!(H: a, b, c, d, m[1], 0xa4beea44, 4);
+            step!(H2: d, a, b, c, m[4], 0x4bdecfa9, 11);
+            step!(H: c, d, a, b, m[7], 0xf6bb4b60, 16);
+            step!(H2: b, c, d, a, m[10], 0xbebfbc70, 23);
+            step!(H: a, b, c, d, m[13], 0x289b7ec6, 4);
+            step!(H2: d, a, b, c, m[0], 0xeaa127fa, 11);
+            step!(H: c, d, a, b, m[3], 0xd4ef3085, 16);
+            step!(H2: b, c, d, a, m[6], 0x04881d05, 23);
+            step!(H: a, b, c, d, m[9], 0xd9d4d039, 4);
+            step!(H2: d, a, b, c, m[12], 0xe6db99e5, 11);
+            step!(H: c, d, a, b, m[15], 0x1fa27cf8, 16);
+            step!(H2: b, c, d, a, m[2], 0xc4ac5665, 23);
+
+            step!(I: a, b, c, d, m[0], 0xf4292244, 6);
+            step!(I: d, a, b, c, m[7], 0x432aff97, 10);
+            step!(I: c, d, a, b, m[14], 0xab9423a7, 15);
+            step!(I: b, c, d, a, m[5], 0xfc93a039, 21);
+            step!(I: a, b, c, d, m[12], 0x655b59c3, 6);
+            step!(I: d, a, b, c, m[3], 0x8f0ccc92, 10);
+            step!(I: c, d, a, b, m[10], 0xffeff47d, 15);
+            step!(I: b, c, d, a, m[1], 0x85845dd1, 21);
+            step!(I: a, b, c, d, m[8], 0x6fa87e4f, 6);
+            step!(I: d, a, b, c, m[15], 0xfe2ce6e0, 10);
+            step!(I: c, d, a, b, m[6], 0xa3014314, 15);
+            step!(I: b, c, d, a, m[13], 0x4e0811a1, 21);
+            step!(I: a, b, c, d, m[4], 0xf7537e82, 6);
+            step!(I: d, a, b, c, m[11], 0xbd3af235, 10);
+            step!(I: c, d, a, b, m[2], 0x2ad7d2bb, 15);
+            step!(I: b, c, d, a, m[9], 0xeb86d391, 21);
+
             a0 = a0.wrapping_add(a);
             b0 = b0.wrapping_add(b);
             c0 = c0.wrapping_add(c);

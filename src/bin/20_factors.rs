ef860afec8f433e19d6b041b0eb4ec396a7f4b3b
Peter Tseng <pht24@cornell.edu>
2018-12-31 13:05:21 +0000

    day 20: lower bound with Robin's inequality
    Barely makes a difference. Runtime 1.3 seconds -> 1.1 seconds

--- src/bin/20_factors.rs
+++ src/bin/20_factors.rs
@@ -1,6 +1,30 @@
+// Euler-Mascheroni constant
+#[allow(clippy::unreadable_literal)]
+const GAMMA: f64 = 0.5772156649015329;
+
+fn house_lower_bound(target: usize) -> usize {
+    // Robin's inequality:
+    // \sigma(n) < e^\gamma n \log \log n
+    // n \log \log n > \frac{T}{e^\gamma}
+    // lower bound, so \log \log n can be increased to \log \log T
+    // n > \frac{T}{e^\gamma \log \log T}
+
+    let n = (target as f64 / (GAMMA.exp() * (target as f64).ln().ln())).ceil() as usize;
+
+    // TODO: This was approximate (since we changed an n for T) and we can do better,
+    // but the improvement is unlikely to be significant (704242 -> 733346, 641725 -> 668446).
+
+    // Robin's inequality doesn't hold for n <= 5040.
+    if n > 5040 {
+        n
+    } else {
+        1
+    }
+}
+
 fn first_house(target: usize, per_elf: u8, limit: Option<usize>) -> usize {
     let elf_factor_needed = target / usize::from(per_elf);
-    let lower_bound = 1;
+    let lower_bound = house_lower_bound(elf_factor_needed);
     let upper_bound = elf_factor_needed;
     let mut presents = vec![usize::from(per_elf); upper_bound + 1 - lower_bound];
 
